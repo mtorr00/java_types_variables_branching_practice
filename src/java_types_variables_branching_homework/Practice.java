a2e820d77399cebb1888d12732f06990c0d2ff23
martintorrey <hockeymt.torrey@gmail.com>
2022-04-18 10:57:34 -0400

    added practice file

---
+++ src/java_types_variables_branching_homework/Practice.java
@@ -0,0 +1,18 @@
+package java_types_variables_branching_homework;
+public class Practice {
+	public static void main(String args[]) {
+//		String blank;
+//		System.out.println(blank);
+//		int $deal;
+//		int _attaboy;
+//		System.out.println(_attaboy);
+//		
+//		//double public;
+//		
+//		System.out.println("hello " + $deal);
+//		
+		
+//		int zero = 0;
+//		System.out.println(1 / zero);
+	}
+}
